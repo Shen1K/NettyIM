dce1c215666b174482a5f671b9a423527614ad32
user <2997735722@qq.com>
2019-06-25 21:51:55 +0800

    消息回复

---
+++ src/main/java/packet/response/MessageResponsePacket.java
@@ -0,0 +1,21 @@
+package packet.response;
+
+import packet.Command;
+import packet.Packet;
+
+public class MessageResponsePacket extends Packet {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public Byte getCommand() {
+        return Command.MESSAGE_RESPONSE;
+    }
+}
