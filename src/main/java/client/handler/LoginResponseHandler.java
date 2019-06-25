7edba2e5a81a2fc6b5c7525a7a9d3b0e5d95209e
user <2997735722@qq.com>
2019-06-25 22:28:25 +0800

    消息的处理逻辑

---
+++ src/main/java/client/handler/LoginResponseHandler.java
@@ -0,0 +1,33 @@
+package client.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import packet.request.LoginRequestPacket;
+import packet.response.LoginResponsePacket;
+import util.Util;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class LoginResponseHandler extends SimpleChannelInboundHandler<LoginResponsePacket> {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        LoginRequestPacket loginRequestPacket = new LoginRequestPacket();
+        loginRequestPacket.setName("sk");
+        loginRequestPacket.setUserId(UUID.randomUUID().toString());
+        loginRequestPacket.setPassword("pwd");
+
+        ctx.channel().writeAndFlush(loginRequestPacket);
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, LoginResponsePacket msg) throws Exception {
+        if(msg.isSuccess()){
+            System.out.println(new Date() + ": 客户端登录成功");
+            Util.markAsLogin(ctx.channel());
+        }else {
+            System.out.println(new Date() + ": 客户端登录失败，原因：" + msg.getReason());
+        }
+    }
+}
---
+++ src/main/java/server/handler/LoginRequestHandler.java
@@ -0,0 +1,31 @@
+package server.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import packet.request.LoginRequestPacket;
+import packet.response.LoginResponsePacket;
+
+import java.util.Date;
+
+public class LoginRequestHandler extends SimpleChannelInboundHandler<LoginRequestPacket> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, LoginRequestPacket msg) throws Exception {
+        System.out.println(new Date() + ": ÊÕµ½¿Í»§¶ËµÇÂ¼ÇëÇó¡­¡­");
+        LoginResponsePacket loginResponsePacket = new LoginResponsePacket();
+        loginResponsePacket.setVersion(msg.getVersion());
+
+        if(valid(msg)){
+            loginResponsePacket.setSuccess(true);
+            System.out.println(new Date() + "£ºµÇÂ¼³É¹¦£¡");
+        }else {
+            loginResponsePacket.setReason("µÇÂ¼Ê§°Ü");
+            loginResponsePacket.setSuccess(false);
+            System.out.println(new Date() + "£ºµÇÂ¼Ê§°Ü");
+        }
+        ctx.channel().writeAndFlush(loginResponsePacket);
+    }
+
+    private boolean valid(LoginRequestPacket loginRequestPacket) {
+        return true;
+    }
+}
