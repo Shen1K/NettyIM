b6afd682db4be95760f11c5356d08f6e63e39132
user <2997735722@qq.com>
2019-06-25 22:03:42 +0800

    消息发送

---
+++ src/main/java/packet/request/MessageRequestPacket.java
@@ -0,0 +1,21 @@
+package packet.request;
+
+import packet.Command;
+import packet.Packet;
+
+public class MessageRequestPacket extends Packet {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public Byte getCommand() {
+        return Command.MESSAGE_REQUEST;
+    }
+}
